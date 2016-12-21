37533c92b85702c7521079e363084c04a2d355a9
Evgeniy Kosjakov <42_@mail.ru>
2016-12-21 16:56:38 +0300

    parallel

---
+++ Ghpr.SpecFlow.Examples/Ghpr.SpecFlow.ParallelExamples/Features/MathFeatures/Basics feature name.feature
@@ -0,0 +1,17 @@
+﻿Feature: Basics feature name
+	In order to avoid silly mistakes
+	As a math idiot
+	I want to be told the sum of numbers
+
+@Addition
+Scenario: Add two numbers
+	Given I have number 50
+	When I add 20
+	Then the result sum should be 70
+
+@Addition
+Scenario: Add three numbers
+	Given I have number 10
+	When I add 20
+	And I add 7
+	Then the result sum should be 37
---
+++ Ghpr.SpecFlow.Examples/Ghpr.SpecFlow.ParallelExamples/Steps/BasicsSteps.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Diagnostics;
+using NUnit.Framework;
+using TechTalk.SpecFlow;
+
+namespace Ghpr.SpecFlow.Examples.Steps
+{
+    [Binding]
+    public class BasicsSteps
+    {
+        private int _firstNumber;
+        private int _sum;
+
+        [Given(@"I have number (.*)")]
+        public void GivenIHaveNumber(int p0)
+        {
+            Trace.WriteLine("TRACE: I have some number...");
+            Console.WriteLine("CONSOLE: I have some number...");
+            Debug.WriteLine("DEBUG: I have some number...");
+            _firstNumber = p0;
+            _sum = p0;
+        }
+
+        [When(@"I add (.*)")]
+        public void WhenIAdd(int p0)
+        {
+            _sum += p0;
+        }
+
+        [Then(@"the result sum should be (.*)")]
+        public void ThenTheResultShouldBe(int p0)
+        {
+            Assert.AreEqual(p0, _sum);
+        }
+    }
+}
